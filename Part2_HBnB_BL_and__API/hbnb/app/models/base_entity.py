6ae80dd546a142285cff5dbb2de0e9e56226542d
SDINAHET <stephane.dinahet@gmail.com>
2024-10-14 12:02:05 +0200

    add file base_entity.py in folder models.

---
+++ Part2_HBnB_BL_and__API/hbnb/app/models/base_entity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import uuid
+from datetime import datetime
+
+class BaseEntity:
+    def __init__(self):
+    self.id = str(uuid.uuid4())
+    self.created_at = datetime.now()
+    self.updated_at = datetime.now()
+
+    def save(self):
+    """Update the updated_at timestamp whenever the object is modified"""
+    self.updated_at = datetime.now()
+
+    def update(self, data):
+    """Update the attributes of the object based on the provided dictionary"""
+    for key, value in data.items():
+        if hasattr(self, key):
+            setattr(self, key, value)
+    self.save()  # Update the updated_at timestamp
--- Part2_HBnB_BL_and__API/hbnb/app/models/place.py
+++ Part2_HBnB_BL_and__API/hbnb/app/models/place.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+
+class Place(BaseEntity):
+    def __init__(self, title, description, price, latitude, longitude, owner):
+        super().__init__()
+        self.title = title
+        self.description = description
+        self.price = price
+        self.latitude = latitude
+        self.longitude = longitude
+        self.owner = owner
+        self.reviews = []  # List to store related reviews
+        self.amenities = []  # List to store related amenities
+
+    def add_review(self, review):
+        """Add a review to the place."""
+        self.reviews.append(review)
+
+    def add_amenity(self, amenity):
+        """Add an amenity to the place."""
+        self.amenities.append(amenity)
