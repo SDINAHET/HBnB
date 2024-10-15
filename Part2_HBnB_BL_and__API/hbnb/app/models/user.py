e035586fc313152e789c1fd78eafde07f5f49425
Gromuf <luigi.beaumois@gmail.com>
2024-10-15 15:18:33 +0200

    user.py 1

--- Part2_HBnB_BL_and__API/hbnb/app/models/user.py
+++ Part2_HBnB_BL_and__API/hbnb/app/models/user.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
 
 # app/models/user.py
+from base_entity import BaseEntity
 import re
 
 
 class User(BaseEntity):
+
+    def __init__(self, firstName, lastName, email, password, isAdmin):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.password = password
+        self.isAdmin = isAdmin
+
+
+    def registerProfile(self):
+        pass
+
+
+    def updateProfile(self):
+        pass
+
+    # def deleteProfile(self):
+    #     pass
