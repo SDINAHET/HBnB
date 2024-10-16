ba71faadf92f1cbc50df37a4f3a8ff9fede1c520
Gromuf <luigi.beaumois@gmail.com>
2024-10-16 10:42:17 +0200

    user class maybe not finished

--- Part2_HBnB_BL_and__API/hbnb/app/models/user.py
+++ Part2_HBnB_BL_and__API/hbnb/app/models/user.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 
 # app/models/user.py
-from .base_entity import BaseEntity
+from marshmallow import ValidationError
 import re
-import importlib.util
+from typing import List, Dict
+from .base_entity import BaseEntity
+from .place import Place
+from .review import Review
+
 
 
 class User(BaseEntity):
+
+    users: Dict[str, 'User'] = {} # Class-level storage for users
 
     def __init__(self, firstName, lastName, email, password, isAdmin):
         super().__init__() #appelle le constructeur de BaseEntity
@@ -15,3 +21,21 @@ class User(BaseEntity):
         self.email = email
         self.password = password
         self.isAdmin = isAdmin
+        self.places: List['Place'] = []  # Places owned by the user
+        self.reviews: List['Review'] = []  # Reviews written by the user
+        self.validate()
+        User.users[self.id] = self
+
+    def validate(self):
+        if not self.first_name or not self.last_name:
+            raise ValidationError("First name and last name cannot be empty.")
+        if not self.is_valid_email(self.email):
+            raise ValidationError("Invalid email format.")
+        if not self.password or len(self.password) < 6:
+            raise ValidationError("Password must be at least 6 characters long.")
+
+    @staticmethod
+    def is_valid_email(email: str) -> bool:
+        # Simple regex for email validation
+        regex = r'^\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        return re.match(regex, email) is not None
