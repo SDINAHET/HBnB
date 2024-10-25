96dd0599310a78b60f3779fd6a04ce4143940992
SDINAHET <stephane.dinahet@gmail.com>
2024-10-25 12:34:32 +0200

    update __int__.py bnb/app/__init__.py api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1')

--- Part2_HBnB_BL_and__API/hbnb/app/__init__.py
+++ Part2_HBnB_BL_and__API/hbnb/app/__init__.py
@@ -13,7 +13,8 @@ from app.api.v1.amenities import api as amenities_ns
 
 def create_app():
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
+    api = Api(app, version='1.0', title='HBnB API',
+              description='HBnB Application API', doc='/api/v1')
 
     # Register the user namespace
     api.add_namespace(users_ns, path='/api/v1/users')
