e03cd71090e9f94eafc6467d47835365beca6cbc
SDINAHET <stephane.dinahet@gmail.com>
2024-11-14 04:13:12 +0100

    Add JWT_SECRET_KEY to config and document environment variable setup

--- Part3_HBnB_Auth_&_DB/hbnb/config.py
+++ Part3_HBnB_Auth_&_DB/hbnb/config.py
@@ -4,6 +4,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    JWT_SECRET_KEY = SECRET_KEY  # add SD
     DEBUG = False
 
 class DevelopmentConfig(Config):
@@ -13,3 +14,15 @@ config = {
     'development': DevelopmentConfig,
     'default': DevelopmentConfig
 }
+
+
+
+# pip install python-dotenv
+
+
+# Créez un fichier .env à la racine de votre projet et ajoutez-y les clés sensibles :
+# Dans un fichier .env (qui doit être ajouté à votre .gitignore pour ne pas le versionner), ajoutez votre clé secrète :
+
+# SECRET_KEY=votre_clé_secrète_ici
+
+# load_dotenv()  # Charger les variables d'environnement depuis le fichier .env
