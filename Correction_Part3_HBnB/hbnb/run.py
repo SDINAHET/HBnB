b5143197c3af3996b4325e4f9a82e16ce210c74a
SDINAHET <stephane.dinahet@gmail.com>
2024-11-27 17:20:21 +0100

    Implement authentication endpoints and admin access control; remove unused migration setup

--- Correction_Part3_HBnB/hbnb/run.py
+++ Correction_Part3_HBnB/hbnb/run.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from app import db, create_app
-from flask_migrate import Migrate
+# from flask_migrate import Migrate
 
 # Créer l'application Flask
 app = create_app()
 
-# Configurer Flask-Migrate pour la gestion des migrations
-migrate = Migrate(app, db)
+# # Configurer Flask-Migrate pour la gestion des migrations
+# migrate = Migrate(app, db)
 
 # Point d'entrée pour démarrer le serveur Flask
 if __name__ == "__main__":
