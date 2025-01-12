f801300aca4c66fbddbcaebb5e672c6ae1421302
SDINAHET <stephane.dinahet@gmail.com>
2025-01-12 18:16:01 +0100

    task1 login + id user in cookies + db js  ok

--- Part4_HBnB_Simple_Frontend/backend/app/api/v1/auth copy.py
+++ Part4_HBnB_Simple_Frontend/backend/app/api/v1/auth copy.py
@@ -18,7 +18,6 @@ class Login(Resource):
     @api.expect(login_model)
     @api.response(200, 'Login successful')
     @api.response(401, 'Invalid credentials')
-    @api.response(404, 'User not found')
     def post(self):
         """
         Authenticate user and return a JWT token
@@ -34,12 +33,6 @@ class Login(Resource):
         if not user.verify_password(credentials['password']):
             current_app.logger.error(f"Invalid credentials for user: {credentials['email']}")
             return {'error': 'Invalid credentials'}, 401
-
-        # Créer un JWT token avec l'identité utilisateur
+        
         access_token = create_access_token(identity={'id': str(user.id), 'is_admin': user.is_admin})
-
-        # Retourner le token et l'ID utilisateur dans la réponse
-        return {
-            'access_token': access_token,
-            'user_id': str(user.id)  # Inclure l'ID utilisateur
-        }, 200
+        return {'access_token': access_token}, 200
